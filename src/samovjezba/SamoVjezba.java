ea79fe655a5e9b6ad4e12489abe5f09a191b6e77
vedad33 <50501516+vedad33@users.noreply.github.com>
2019-09-03 11:50:40 +0200

    commit14

---
+++ src/samovjezba/SamoVjezba.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samovjezba;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author wedad
+ */
+public class SamoVjezba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+Pocetni g = new Pocetni();
+g.setVisible(true);
+g.setTitle("Digitron-Calculator");
+g.setAlwaysOnTop(true);
+g.setBounds(600, 200, 500, 700);
+
+        
+
+    }
+
+}
